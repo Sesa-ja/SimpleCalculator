a1a8c11b51d1eb08102b921cafe6297819486e6a
Wasim Jafar <wasim.jafar_2026@ucentralasia.org>
2024-08-15 14:41:18 +0600

    The application is done

---
+++ script.js
@@ -0,0 +1,14 @@
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+function clearDisplay() {
+    document.getElementById("display").value = '';
+}
+function calculate() {
+    let expression = document.getElementById("display").value;
+    try {
+        document.getElementById("display").value = eval(expression);
+    } catch (error) {
+        document.getElementById("display").value = "Error";
+    }
+}
